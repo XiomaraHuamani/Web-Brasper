0ced4a57c919ada7a7548b3af165c65ab21291f2
NIls <alberthxen@gmail.com>
2024-12-06 10:19:05 -0500

    elimianr middleware

--- src/layout/header/Header1.js
+++ src/layout/header/Header1.js
@@ -19,14 +19,14 @@ const Header1 = () => {
             <div className="nav-outer mx-auto clearfix">
               <Menu />
             </div>
-            {/* <div className="menu-btns">
+            <div className="menu-btns">
               <Link legacyBehavior href="login">
                 <a className="theme-btn">
                   {t.header.login} <i className="fas fa-long-arrow-right" />
                 </a>
               </Link>
             </div>
-            <div className="menu-btns">
+           {/*  <div className="menu-btns">
               <Link legacyBehavior href="/singup">
                 <a className="theme-btn style-three">
                   Registrate <i className="fas fa-long-arrow-right" />
